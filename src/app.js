98a91d0e6e6905abd7434190754ab93a6e22469c
Deepak Maddheshiya <maddheshiya.deepak@gmail.com>
2024-10-19 12:04:08 +0530

    HTTP methods and Routers

--- src/app.js
+++ src/app.js
@@ -2,8 +2,34 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Reading file from the server");
+app.get(/.*fly$/, (req, res) => {
+  res.send({ firstname: "Deepak", lastname: "Maddy" });
+});
+
+app.get("/user", (req, res) => {
+  console.log(req.query);
+  res.send({ firstname: "Deepak", lastname: "Maddy" });
+});
+
+app.get("/user/:userId", (req, res) => {
+  console.log(req.params);
+  res.send({ firstname: "Deepak", lastname: "Maddy" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data updated successfully");
+});
+
+app.put("/user", (req, res) => {
+  res.send("Data put successfully");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("Data patched successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data deleted successfully");
 });
 
 app.use("/", (req, res) => {
